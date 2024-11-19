2ddbd99a4dfce8ec421d420ee4c3e188bf0eeae7
Rolando Marin <79164450+RolandoMarin@users.noreply.github.com>
2024-11-18 19:25:45 -0800

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -4,11 +4,11 @@ ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
 // modify these settings according to the account on your database server.
-$host = "localhost";
+$host = "nuskkyrsgmn5rw8c.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	";
 $port = "3306";
-$username = "root";
-$user_pass = "root";
-$database_in_use = "JokesDB";
+$username = "fa6rh2h53fi75c3w";
+$user_pass = "ywriuxizmg199v7g";
+$database_in_use = "x87fwmcgk6fcmfkn";
 
 
 $mysqli = new mysqli($host, $username, $user_pass, $database_in_use);
